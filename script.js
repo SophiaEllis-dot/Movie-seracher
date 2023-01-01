72b25c1763566cb1512f72b8d467f0669a649a16
Sophia Ellis <87676748+SophiaEllis-dot@users.noreply.github.com>
2023-01-01 23:25:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var searchInput = $(".search");
+var itemWrapper = $("main");
+
+function displayMatches(matches) {
+  itemWrapper.html("");
+
+  if (!matches) {
+    itemWrapper.html('<p class="no-search">No results found.</p>');
+  } else {
+    for (var matchObj of matches) {
+      itemWrapper.append(`
+      <div class="movie-item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${matchObj.Poster})">
+        <h3>${matchObj.Title}</h3>
+        <p>Release Year: ${matchObj.Year}</p>
+        <a href="https://www.imdb.com/title/${matchObj.imdbID}" target="_blank">View More Details</a>
+      </div>
+    `);
+    }
+  }
+}
+
+function getMovieData(event) {
+  var keyCode = event.keyCode;
+  var searchText = searchInput.val().trim();
+
+  if (keyCode === 13 && searchText) {
+    $.get(`https://www.omdbapi.com/?apikey=46e568e2&s=${searchText}`).then(
+      function (data) {
+        displayMatches(data.Search);
+      }
+    );
+  }
+}
+
+function init() {
+  searchInput.keydown(getMovieData);
+}
+
+init();
